d94a3f8d0f064e246f2b561426bd92431da5a8db
Nikola Draskovic <draskovic26z@gmail.com>
2022-12-01 23:52:43 +0100

    Pocetak i izgled pocetne stranice

---
+++ glavnaStranica.php
@@ -0,0 +1,97 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Snabdevanje knjizara</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <!-- Navbar content -->
+
+    <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Snabdevanje knjizara</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Pocetna stranica</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Knjige</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Lokacije</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Nabavka knjiga</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            Dodaj
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Nova knjiga</a></li>
+                            <li><a class="dropdown-item" href="#">Nova knjizara</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Poruci knjige</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+
+    <div style="height: 50px"></div>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <div style="height: 50px"></div>
+        <div class="card border border-1 rounded-5 shadow my-5" style="background-color: wheat">
+            <div class="card-body p-4 ">
+                <div style="height: 20px"></div>
+                <h1 class="fw-bolder position-absolute start-50 translate-middle">
+                    Snabdevanje knjizare</h1>
+                <div style="height: 30px"></div>
+                <p class="lead fw-semibold position-absolute top-50 start-50 translate-middle "
+                    style="text-align: center">Dobro došli na sajt za
+                    snabdevanje knjižara. <br /> Ulaskom na stranicu 'Knjige' možete pristupiti svim dostupnim
+                    knjigama. Na stranici 'Lokacije' se nalaze sve knjižare za koje možete poručiti knjige. Klikom
+                    na
+                    stranicu 'Nabavka knjiga' možete videti sve narudzbine. </br> U delu 'Dodaj' se mogu dodati nove
+                    knjige, lokacije, ali takođe i izvršiti nova porudžbina. </p>
+                <div style="height: 120px"></div>
+                <br /><br /><br /><br /><br /><br /><br /><br />
+                <p class="lead mb-0 fw-normal position-absolute start-50 translate-middle" style="text-align: center">
+                    Informacije:
+                    nd20201015@student.fon.bg.ac.rs<br />Kontakt telefon:
+                    +381648239727</p>
+                <br /><br />
+
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
